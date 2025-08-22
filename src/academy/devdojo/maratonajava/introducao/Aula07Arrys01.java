8cd514ee27206b79087f5a8b0b7eb5ab4478e82d
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-08-22 20:35:20 -0300

    aula sobre Arrys em java

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrys01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrys01.java
@@ -2,6 +2,8 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrys01 {
     public static void main(String[] args) {
-
+    int [] idades = new int [3];
+        idades[0]=20;
+        System.out.println(idades[0]);
     }
 }
---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrys02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrys02 {
+    public static void main(String[] args) {
+
+        //byte,short, int ,long, float e doube = 0
+        //boolear false
+        // Srting null
+      float[]  idades =new float[3];
+        System.out.println(idades[0]);
+    }
+}
