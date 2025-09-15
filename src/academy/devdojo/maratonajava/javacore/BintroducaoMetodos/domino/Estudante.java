12c92874bfcc0a6026c7c760d5735eb7bc29ac97
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-09-15 15:05:03 -0300

    Continuação da aula sobre Orientação Objeto aprendendo metados-Parâmetros tipo referência  em java

---
+++ src/academy/devdojo/maratonajava/javacore/BintroducaoMetodos/domino/Estudante.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.BintroducaoMetodos.domino;
+
+public class Estudante {
+        public String nome;
+        public int idade;
+        public char sexo;
+}
+
+
