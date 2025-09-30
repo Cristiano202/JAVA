58d805ae5b956b3665d676fcaa59a08eb7d52b0e
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-09-29 22:15:35 -0300

    Continuação da aula sobre Orientação Objeto aprendendo Crostrutores

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/Test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/Test/AnimeTest01.java
@@ -5,11 +5,11 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime =new Anime("cris","tv",12,"Ação");
-        Anime anime2=new Anime();
+        Anime anime =new Anime("cris","tv",12,"Ação","Produto");
+        //Anime anime2=new Anime();
         //anime.init("cris","tv",12,"Ação");
         anime.emprime();
-        anime2.emprime();
+        //anime2.emprime();
 
     }
 
