cec13be2bfd96c4c5a6f93763de663a580af196d
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-01 21:02:37 -0300

    Aprendo bloco de enicialização

---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicialzacao/Test/AnomeTest.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicialzacao.Test;
+
+import academy.devdojo.maratonajava.javacore.Eblocosinicialzacao.dormain.Anime;
+
+public class AnomeTest {
+    public static void main(String[] args) {
+        Anime anime=new Anime();
+        System.out.println(anime.getEpisodios());
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicialzacao/dormain/Anime.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicialzacao.dormain;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Anime() {
+        episodios=new int[100];
+        int i=0;
+        for (i=0;i<episodios.length;i++){
+            episodios[i]=i+1;
+
+        }
+        for(int episodio :this.episodios){
+            System.out.print(episodio+ " ");
+            System.out.println();
+        }
+
+    }
+}
