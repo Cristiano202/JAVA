9cb2877ee4e1b8724f3d62a94a411994997d0684
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-01 21:08:17 -0300

    Continuação da aula sobre Orientação Objeto aprendendo bloco de inicialização

--- src/academy/devdojo/maratonajava/javacore/Eblocosinicialzacao/dormain/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Eblocosinicialzacao/dormain/Anime.java
@@ -15,7 +15,16 @@ public class Anime {
     public String getNome() {
         return nome;
     }
+//1 - Alocado espaço em memória pro objeto
+        {
+          System.out.println("Dentro do bloco de iniialização");
+            episodios=new int[100];
+            int i=0;
+            for (i=0;i<episodios.length;i++){
+                episodios[i]=i+1;
 
+            }
+        }
     public Anime() {
         episodios=new int[100];
         int i=0;
