01ef19d5aa4f066b52ba053e148b66d52591e7e9
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-08 20:10:16 -0300

    Associação bidirecional

--- src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
@@ -2,6 +2,33 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
 
 public class Time {
     private  String nome;
+    private Jogador[]jogadores;
+
+    public Time(Jogador[] jogadores, String nome) {
+        this.jogadores = jogadores;
+        this.nome = nome;
+    }
+
+    public Time(Jogador[] jogadores) {
+        this.jogadores = jogadores;
+    }
+    public void imprime(){
+        System.out.println(this.nome);
+        if(jogadores==null)return;
+
+        for (Jogador jogadore : jogadores) {
+            System.out.println(jogadore.getNome());
+        }
+
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
+    }
 
     public Time(String nome) {
         this.nome = nome;
