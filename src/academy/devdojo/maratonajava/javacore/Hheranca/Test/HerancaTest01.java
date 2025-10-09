1755e51cf79a6c97b83585de025231148a0ecfa3
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-09 20:21:24 -0300

    Aprendo um pouco sobre herança

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/Test/HerancaTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.Test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco =new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("5892929");
+        Pessoa pessoa =new Pessoa();
+        pessoa.setNome("Cris");
+        pessoa.setNome("123444444");
+        pessoa.setEndereco(endereco);
+        pessoa.emprime();
+
+        Funcionario funcionario =new Funcionario();
+        funcionario.setNome("Abel balas");
+        funcionario.setCpf("12356789");
+        funcionario.setSalario(1234);
+        funcionario.setEndereco(endereco);
+        System.out.println("---------");
+        funcionario.emprime();
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Endereco.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Endereco {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private  String rua;
+    private String cep;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+
+}
