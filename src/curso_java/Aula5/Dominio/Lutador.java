f650b35c61af62e759024c2906679313918021a5
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-13 21:10:15 -0300

    Praticando Poo

---
+++ src/curso_java/Aula5/Dominio/Lutador.java
@@ -0,0 +1,131 @@
+package curso_java.Aula5.Dominio;
+
+public class Lutador {
+    private String nome;
+    private  String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitoria;
+    private int derrotas;
+    private int empates;
+
+    public Lutador(String nome, String nacionalidade, int idade, float altura,
+                   float peso, int vitoria, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitoria = vitoria;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if(this.peso<52.2){
+            this.categoria="Invalido";
+        }
+        else if(this.peso<=70.3) {
+            this.categoria = "Leve";
+        }
+        else if(this.peso<=83.9){
+            this.categoria="Medio";
+
+        } else if (this.peso<=120.2) {
+            this.categoria="Pesado";
+        }
+        else {
+            this.categoria="Invalido";
+        }
+
+    }
+
+    public int getVitoria() {
+        return vitoria;
+    }
+
+    public void setVitoria(int vitoria) {
+        this.vitoria = vitoria;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public void apresentar(){
+
+
+    }
+    public void status(){
+
+    }
+    public void ganharLuta(){
+
+
+    }
+    public void perderLuta(){
+
+    }
+    public void empatarLuta(){
+
+    }
+}
