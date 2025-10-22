c64db37e090aabe90fa25689c9d62c326ba634a3
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-21 21:14:36 -0300

    emplementando os metados

--- src/curso_java/AulaX/dominio/Pessoa.java
+++ src/curso_java/AulaX/dominio/Pessoa.java
@@ -5,4 +5,34 @@ public class Pessoa {
     private int idade;
     private  String sexo;
 
+
+    public void fazerAniversario(){
+        this.idade++;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+
+
 }
