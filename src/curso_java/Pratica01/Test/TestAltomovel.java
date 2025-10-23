0164f171ca0d3c06ad28b6964395b198f5aa83ef
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-23 14:20:50 -0300

    Fazendo melhoria no software  e fazendo teste

--- src/curso_java/Pratica01/Test/TestAltomovel.java
+++ src/curso_java/Pratica01/Test/TestAltomovel.java
@@ -10,16 +10,24 @@ public class TestAltomovel {
         Scanner sc =new Scanner(System.in);
         Carro c1=new Carro();
         Moto m1=new Moto();
-
+        int res=0;
         do{
             System.out.println("1-CADASTRAR MOTO");
             System.out.println("2-CADASTRAR CARRO");
             System.out.println("0-Sair");
             System.out.print("Informe qual altomovel você quer cadastrar: ");
-            int res= sc.nextInt();
+            res= sc.nextInt();
             sc.nextLine();
-            if ()
-        }
+            if (res==1){
+                System.out.print("Informe o nome: ");
+                m1.setNome(sc.nextLine());
+                System.out.print("Informe a placa: ");
+                m1.setPlaca(sc.nextInt());
+                sc.nextLine();
+                System.out.print("Informe o modelo: ");
+                m1.setModelo(sc.nextLine());
+            }
+        }while(res !=0);
     }
 
 
