cb86cb1e39bc6bfdf7f93b01954e04d0a7ee9354
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-23 20:11:56 -0300

    Emplementando classe peixe

---
+++ src/curso_java/Aula12/Dominio/Peixe.java
@@ -0,0 +1,22 @@
+package curso_java.Aula12.Dominio;
+
+public class Peixe extends Animal{
+    private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println("Nadando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo substâncias");
+
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Soltou uma bolha");
+
+    }
+}
