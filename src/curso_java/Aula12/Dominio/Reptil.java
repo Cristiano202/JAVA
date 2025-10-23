5fce2c5130acc2e4ea525b1b4dc1191a46f4b140
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-23 20:09:16 -0300

    Emplementando classe peixe

---
+++ src/curso_java/Aula12/Dominio/Reptil.java
@@ -0,0 +1,22 @@
+package curso_java.Aula12.Dominio;
+
+public class Reptil extends Animal{
+    private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println("Rastejando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo vegetais");
+
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de réptil");
+
+    }
+}
