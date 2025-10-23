9ec7573fff6cc1b5a49302d0bb1cc5a02d5fb50d
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-10-23 14:05:50 -0300

    emplementando os metados

---
+++ src/curso_java/Pratica01/Test/TestAltomovel.java
@@ -0,0 +1,10 @@
+package curso_java.Pratica01.Test;
+
+import curso_java.Pratica01.Dominio.Carro;
+import curso_java.Pratica01.Dominio.Moto;
+
+public class TestAltomovel {
+    Carro c1=new Carro();
+    Moto m1=new Moto();
+
+}
