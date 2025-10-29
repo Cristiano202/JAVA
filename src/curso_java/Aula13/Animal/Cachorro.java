4359405b3ca4af62e81a4834fa7d0500cf3686b3
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-29 11:36:59 -0300

    fazendo melhoria e emplemando sobre posisao no software e fazendo o testes..

--- src/curso_java/Aula13/Animal/Cachorro.java
+++ src/curso_java/Aula13/Animal/Cachorro.java
@@ -5,5 +5,28 @@ public class Cachorro extends Lobo{
     public void emitirSom(){
         System.out.println("Au Au Au ");
 
+
     }
+
+    public void reagir(String frase){
+        if(frase.equals("Toma comida")|| frase.equals("Ola")){
+            System.out.println("Abanar e latir");
+        }
+        else {
+            System.out.println("Rosnar");
+        }
+
+    }
+    public void reagir(int hora, int min){
+
+    }
+    public void reagir(Boolean dono) {
+
+    }
+    public void reagir(int idade,float peso){
+
+    }
+
+
+
 }
