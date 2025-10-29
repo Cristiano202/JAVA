4727bff5d70da06e53a1e89d4541f6f2757e76ec
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-29 12:10:29 -0300

    Emplementando metados para cachorro

--- src/curso_java/Aula13/Animal/Cachorro.java
+++ src/curso_java/Aula13/Animal/Cachorro.java
@@ -30,9 +30,33 @@ public class Cachorro extends Lobo{
 
     }
     public void reagir(Boolean dono) {
+        if (dono){
+            System.out.println("Abanar");
+        }
+        else {
+            System.out.println("Rosnar e latir");
+        }
 
     }
     public void reagir(int idade,float peso){
+        if(idade <5){
+            if(peso < 10){
+                System.out.println("Abanar");
+            }
+            else {
+                System.out.println("Latir");
+            }
+        }
+        else {
+            if (peso<10){
+                System.out.println("Rosnar");
+            }
+            else {
+                System.out.println("Ignorar");
+            }
+
+
+        }
 
     }
 
