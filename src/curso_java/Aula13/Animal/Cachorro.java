e6ac9eb8e5e771d8cbbbac1f45a65082fc79f0f3
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-29 11:18:02 -0300

    fazendo melhoria e emplemando sobre posisao no software e fazendo o testes..

--- src/curso_java/Aula13/Animal/Cachorro.java
+++ src/curso_java/Aula13/Animal/Cachorro.java
@@ -1,4 +1,9 @@
 package curso_java.Aula13.Animal;
 
 public class Cachorro extends Lobo{
+    @Override
+    public void emitirSom(){
+        System.out.println("Au Au Au ");
+
+    }
 }
--- src/curso_java/Aula13/Test/Main.java
+++ src/curso_java/Aula13/Test/Main.java
@@ -1,7 +1,17 @@
 package curso_java.Aula13.Test;
 
+import curso_java.Aula13.Animal.Cachorro;
+import curso_java.Aula13.Animal.Lobo;
+import curso_java.Aula13.Animal.Mamifero;
+
 public class Main {
     public static void main(String[] args) {
+        Lobo lb=new Lobo();
+        Cachorro cc=new Cachorro();
+        Mamifero mm=new Mamifero();
+
+        lb.emitirSom();
+        cc.emitirSom();
 
     }
 }
