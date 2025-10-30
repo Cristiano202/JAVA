37f103105f1e1a06e84f973422766610b5178ee4
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-30 15:54:31 -0300

    Praticando Poo

--- src/curso_java/ProjetoYoutube/Video.java
+++ src/curso_java/ProjetoYoutube/Video.java
@@ -57,16 +57,19 @@ public class Video implements AcoesVideo{
 
     @Override
     public void play() {
+        this.reproduzindo=true;
 
     }
 
     @Override
     public void pause() {
+        this.reproduzindo=false;
 
     }
 
     @Override
     public void like() {
+        this.curtidas++;
 
     }
 
