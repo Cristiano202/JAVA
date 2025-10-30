45f605f93b07a3397871353face5df73b7959f3a
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-30 15:35:15 -0300

    Emplementando Classes

---
+++ src/curso_java/ProjetoYoutube/AcoesVideo.java
@@ -0,0 +1,8 @@
+package curso_java.ProjetoYoutube;
+
+public interface AcoesVideo {
+    public void play();
+    public void pause();
+    public void like();
+
+}
---
+++ src/curso_java/ProjetoYoutube/ProjetoYoutube.java
@@ -0,0 +1,11 @@
+package curso_java.ProjetoYoutube;
+
+public class ProjetoYoutube {
+    public static void main(String[] args) {
+        Video video[]=new Video[3];
+        video[0]=new Video("Aula 1 de poo");
+        video[1]=new Video("Aula 2 de poo");
+        video[2]=new Video("Aula 3 de poo");
+
+    }
+}
