2425186909fc32d27351805340db136efcd46f0b
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-10-30 15:50:45 -0300

    Emplementando Classes Funcionario

--- src/curso_java/ProjetoYoutube/ProjetoYoutube.java
+++ src/curso_java/ProjetoYoutube/ProjetoYoutube.java
@@ -7,6 +7,11 @@ public class ProjetoYoutube {
         video[1]=new Video("Aula 2 de poo");
         video[2]=new Video("Aula 3 de poo");
 
+        Funcionario funcionario[]=new Funcionario[20];
+
+        funcionario[0]=new Funcionario("Cris",19,"M",12,"crisMMM");
+        System.out.println(funcionario[0].toString());
+
         System.out.println(video[0].toString());
 
     }
