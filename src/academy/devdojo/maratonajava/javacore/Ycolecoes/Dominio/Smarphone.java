fcccd830d0253a7e91eba2281ac49947ab62633e
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-11-19 19:33:33 -0300

    Aula sobre equals

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/Dominio/Smarphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/Dominio/Smarphone.java
@@ -8,6 +8,17 @@ public class Smarphone {
         this.seriaNumber = seriaNumber;
         this.marca = marca;
     }
+//Reflexivo:x.equals(x) tem quer ser true para tudo que for de null
+//Simetrico: para x e y diferentes de null , se x.equals(y) == true logo,   y .equals ==true
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)return false;//primeiro tem quer fazer um teste para verificar  se o obj é nulo
+        if (this == obj)return  true;
+        if(this.getClass()!=obj.getClass()) return false;
+        Smarphone smarphone =(Smarphone) obj;
+        return  seriaNumber != null && seriaNumber.equals(smarphone.seriaNumber);
+    }
+
 
     public String getSeriaNumber() {
         return seriaNumber;
