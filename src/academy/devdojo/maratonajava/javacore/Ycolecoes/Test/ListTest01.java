425045b2888802efb7811c7f7a95230245c775f3
Cristiano de carvalho <20cristianogoncalves20.22@gamial.com>
2025-11-21 17:20:05 -0300

    Melhorando o codigo

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/Test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/Test/ListTest01.java
@@ -2,11 +2,26 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ListTest01 {
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         List<String> nomes=new ArrayList<>();
-        nomes.add("cris");
+        int op;
+        String nomePessoa;
+        do{
+            System.out.println("1-Deseja adicionar alguem mais: ");
+            System.out.println("0-Digite zero para sair: ");
+            op= sc.nextInt();
+            sc.nextLine();
+            if(op==1){
+                System.out.print("Digite um nome: ");
+                nomePessoa= sc.nextLine();
+                nomes.add(nomePessoa);
+            }
+        }while (op !=0);
+
         for(String nome : nomes){
             System.out.println(nome);
         }
