f7d9bc253b15a848e194091a2b08803700ecaddc
Cristiano <20cristianogoncalves20.22@gmail.com>
2025-12-25 16:03:19 -0300

    Praticas usando HashMap

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/Test/MapTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.Test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.Dominio.Consumidor;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 =new Consumidor("Cris");
+        System.out.println(consumidor1);
+    }
+}
