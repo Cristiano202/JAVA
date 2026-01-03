dfe31fcb31fd3b992bfb3b5d611540b731d9075e
Cristiano <20cristianogoncalves20.22@gmail.com>
2026-01-03 17:11:22 -0300

    Pratica sobre Map

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/Test/MapTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/Test/MapTest02.java
@@ -1,10 +1,27 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.Test;
 
 import academy.devdojo.maratonajava.javacore.Ycolecoes.Dominio.Consumidor;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.Dominio.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapTest02 {
     public static void main(String[] args) {
         Consumidor consumidor1 =new Consumidor("Cris");
-        System.out.println(consumidor1);
+        Consumidor consumidor2 =new Consumidor("Mari");
+        Consumidor consumidor3 =new Consumidor("pepa");
+
+       Manga manga1= new Manga(2,"ONE PUNCH MAIN",20.00,99);
+       Manga manga2= new Manga(2,"ONE PUNCH MAIN",20.00,99);
+       Manga manga3= new Manga(2,"ONE PUNCH MAIN",20.00,99);
+       Manga manga4=new Manga(2,"ONE PUNCH MAIN",20.00,99);
+       Map<Consumidor,Manga> consumidorManga=new HashMap<>();
+       consumidorManga.put(consumidor1,manga1);
+       consumidorManga.put(consumidor2,manga2);
+       consumidorManga.put(consumidor3,manga2);
+
+       for(Map.Entry<Consumidor , Manga >entry :consumidorManga.entrySet() )
+           System.out.println(entry.getKey() + " - " +entry.getValue() );
     }
 }
