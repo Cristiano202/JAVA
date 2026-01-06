c9926f0295381bd20f269e169a00dd8932f31b2d
Cristiano <20cristianogoncalves20.22@gmail.com>
2026-01-06 20:07:48 -0300

    Um pouco sobre thread

--- src/academy/devdojo/maratonajava/javacore/ZZthreadTest01/ThreadTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZthreadTest01/ThreadTest01.java
@@ -1,7 +1,29 @@
 package academy.devdojo.maratonajava.javacore.ZZthreadTest01;
 
+class ThreadExample extends Thread {
+    private char c;
+
+    public ThreadExample (char c ){
+        this.c=c;
+    }
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+
+        for(int i=0;i<1000;i++){
+            System.out.println(c);
+            if (i%100==0){
+                System.out.println();
+            }
+        }
+    }
+}
+
 public class ThreadTest01 {
+
     public static void main(String[] args) {
-        System.out.println();
+        ThreadExample t1 =new ThreadExample('m');
+        t1.run();
+
     }
 }
